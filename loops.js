4982429453b00bdf14d9ce5775445709febe556e
Paul <pki@midmaine.com>
2018-04-04 14:18:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,15 @@
+function  makeArray() {
+  const array =[0, 1, 2, 3]
+} 
+
 function forLoop() {
   for (let i = 0; i <25; i++) {
-    if ( i === 1) {
-    console.log ("I am 1 strange loop.")
-      }else {console.log ("I am ${i} strange loops.")}
-      return forLoop
+   if(i === 1) {
+     console.log("I am 1 strange loop.")
+   }
+   else {
+  console.log('I am ${i} strange loops')
+   }
+      return makeArray
   }
 }
